e8e34e43bd44a90887226e1d7cf711afcf60bfd3
Mike E <me@michaeleisemann.com>
2021-04-07 22:16:10 -0400

    remove vscode and I NEED TO LEARN how 2 model

--- models/Paste.js
+++ models/Paste.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
-    postId: String,
-    hash: String,
-    created: Date,
-    lastAccess: Date,
-    views: Number,
+  postId: String,
+  hash: String,
+  created: Date,
+  lastAccess: Date,
+  views: Number,
 });
 
 module.exports = mongoose.model("Paste", schema);
